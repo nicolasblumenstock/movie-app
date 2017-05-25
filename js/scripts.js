fade3f3eb27a3fe1a6d5f6098ca3b5284d5d0c97
Nicolas Blumenstock <nicpblum@gmail.com>
2017-05-25 15:22:37 -0400

    added genres

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,58 @@ $(document).ready(function(){
 	const imageBaseUrl = 'http://image.tmdb.org/t/p/';
 	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+apiKey;
 
+	genreButtons();
+
+
 	$.getJSON(nowPlayingUrl,(nowPlayingData)=>{
+		// for (let i = 0; i < genreArray.length; i++){
+		// 	buttonsHTML += `<button class='btn btn-success genre-button'>${genreArray[i].name}</button>`;
+		// 	}
+		// $('#genre-buttons').html(buttonsHTML);
 		var nowPlaying = getHTML(nowPlayingData);
 		$('#movie-grid').html(nowPlaying);
-		console.log(nowPlayingData)
 		$('.movie-poster').click(function(event){
 			var movieID = $(this).attr('movie-id');	
 			movieModal(nowPlayingData,movieID);		
 		})
+		$grid = $('#movie-grid').isotope({
+			itemSelector: '.movie-poster'
+		});
+		$('.genre-button').click(function(event){
+			$grid.isotope({filter: `.${this.innerText}`})
+		})				
 	})
+
+
 
 	$('#movie-form').submit((event)=>{
 		event.preventDefault();
 		var userInput = encodeURI($('#search-input').val());
 		$('#search-input').val('');
 		var searchUrl = `${apiBaseUrl}/search/movie?query=${userInput}&api_key=${apiKey}`;
+		
 		$.getJSON(searchUrl,(searchMovieData)=>{
-			var searchMovie = getHTML(searchMovieData);
+			var searchMovie = getHTML(searchMovieData);			
 			$('#movie-grid').html(searchMovie);
+
 			$('.movie-poster').click(function(event){
 				var movieID = $(this).attr('movie-id');
-				movieModal(searchMovieData,movieID);		
-			})
+				movieModal(searchMovieData,movieID);				
+			});
+			$('#movie-grid').isotope('destroy');
+
+			$grid = $('#movie-grid').isotope({
+				itemSelector: '.movie-poster'
+				});
+			$('.genre-button').click(function(event){
+				$grid.isotope({filter: `.${this.innerText}`})
+				})				
+
+			// sortPosters();
+
 		})
 	})
+
 
 
 	function getHTML(data){
@@ -37,9 +65,17 @@ $(document).ready(function(){
 				var release = data.results[i].release_date;
 				var title = data.results[i].title;
 				var id = data.results[i].id;
+				var thisMovieGenres = data.results[i].genre_ids;
+				var movieGenreClassList = " ";
+				for (let j = 0; j < genreArray.length; j++){
+					if (thisMovieGenres.indexOf(genreArray[j].id) > -1){
+						movieGenreClassList += genreArray[j].name + ' ';
+					}
+				};
+				console.log(movieGenreClassList)
 				var backdrop = `${imageBaseUrl}w780${data.results[i].backdrop_path}`;
 				var movieUrl = `${apiBaseUrl}/movie/${id}?api_key=${apiKey}`;
-				movieHTML += `<div class='col-sm-6 col-md-4 col-lg-3 movie-poster' movie-id=${id} data-toggle='modal' data-target='#myModal'>`;
+				movieHTML += `<div class='col-sm-6 col-md-4 col-lg-3 movie-poster${movieGenreClassList}' movie-id=${id} data-toggle='modal' data-target='#myModal'>`;
 					movieHTML += `<img src="${poster}"/>`;
 				movieHTML += '</div>';
 			}
@@ -54,5 +90,15 @@ $(document).ready(function(){
 			$('.modal-body').html(`<div class="backdrop"><img src="${backdrop}" /></div><div class="release"><span class="info">Release Date: </span>${movieData.release_date}</div><div class="synopsis">${movieData.overview}</div>`);
 		})
 	}
+
+	function genreButtons(){
+		var buttonsHTML = '';		
+		for (let i = 0; i < genreArray.length; i++){
+			buttonsHTML += `<button class='btn btn-success genre-button'>${genreArray[i].name}</button>`;
+		}
+		$('#genre-buttons').html(buttonsHTML);
+	}
+
+
 
 })
